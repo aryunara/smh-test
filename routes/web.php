d91bdc8bf6bd3e5a11194f3b9c82aebebed60d37
aryuna <rabdanova.aryuna@mail.ru>
2024-08-23 19:12:46 +0900

    + api роуты, изменен файл README.md

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('createIPhones', [ProductController::class, 'createIPhones']);
-Route::post('addIPhone', [ProductController::class, 'addProduct']);
